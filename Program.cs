60d494e6056b6d2fdb82240c91a7d12d27c384a7
Iriska-A <72428513+Iriska-A@users.noreply.github.com>
2020-10-09 19:47:10 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static int Main(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] agres)
+        {
+            int c = Main(55, 10);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+         
+        }
+}
+    
+}
